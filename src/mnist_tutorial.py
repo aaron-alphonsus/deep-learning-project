d1f757d7ced1f86baa77c7c1a11e2026dd7d58d4
Aaron Alphonsus <aaron.alphonsus@gmail.com>
2018-04-21 06:03:08 -0600

    Added code for cnn. Will separate out softmax code

--- src/mnist_tutorial.py
+++ src/mnist_tutorial.py
@@ -1,42 +1,128 @@
-# Download and read MNIST data
+# # ------------------------Download and read MNIST data--------------------------
+# 
 from tensorflow.examples.tutorials.mnist import input_data
-# mnist is a lightweight class that stores training, validation and testing sets
-# as NumPy arrays. Also provides function for iterating through data minibatches
+# # mnist is a lightweight class that stores training, validation and testing sets
+# # as NumPy arrays. Also provides function for iterating through data minibatches
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True) # one_hot = binary
-
+# 
+# # ---------------------Start TensorFlow InteractiveSession----------------------
+# 
 import tensorflow as tf
 sess = tf.InteractiveSession() # Allows more flexible structuring of code
-
-# Create placeholder nodes for input images and target classes
+# 
+# # -------------------------Placeholders and Variables---------------------------
+# 
+# # Create placeholder nodes for input images and target classes
 x = tf.placeholder(tf.float32, shape=[None, 784]) # 28*28 image linearized
 y_ = tf.placeholder(tf.float32, shape=[None, 10]) # digit class (0 to 9)
+# 
+# # Define weights W and biases b (provides initial values, tensors full of zeros)
+# W = tf.Variable(tf.zeros([784, 10]))
+# b = tf.Variable(tf.zeros([10]))
+# 
+# # Initialize all variables (W and B here) before using them within session
+# sess.run(tf.global_variables_initializer())
+# 
+# # Implement regression model (output = input images * weights + bias)
+# y = tf.matmul(x, W) + b
+# 
+# # Define the loss function (cross-entropy between target and softmax activation)
+# cross_entropy = tf.reduce_mean(
+#     tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+# 
+# # -----------------------------Training the model-------------------------------
+# 
+# # Steepest gradient descent, step size = 0.5
+# train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+# # repeatedly running train_step trains the model
+# for _ in range(1000):
+#     # 100 training examples in each iteration
+#     batch = mnist.train.next_batch(100)
+#     # replace placeholders with actual training examples
+#     train_step.run(feed_dict = {x: batch[0], y_: batch[1]})
+# 
+# # -----------------------------Evaluating the Model-----------------------------
+# 
+# # Checks whether the output y matches the target y_. Gives a list of booleans.
+# correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+# # Convert booleans to floats and take the mean to get accuracy
+# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# print(accuracy.eval(feed_dict={x: mnist.test.images, y_:mnist.test.labels}))
 
-# Define weights W and biases b (provides initial values, tensors full of zeros)
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
+# -----------------------------Weight Initialization----------------------------
 
-# Initialize all variables (W and B here) before using them within session
-sess.run(tf.global_variables_initializer())
+# Initializing weights with a little noise
+# This is done for 'symmetry breaking' and to prevent 0 gradients
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
 
-# Implement regression model (output = input images * weights + bias)
-y = tf.matmul(x, W) + b
+# Adding slightly positive bias to prevent ReLU neurons from going dead
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
-# Define the loss function (cross-entropy between target and softmax activation)
-cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+# ---------------------------Convolution and Pooling----------------------------
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-# Training the model - Steepest gradient descent, step size = 0.5
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-# repeatedly running train_step trains the model
-for _ in range(1000):
-    # 100 training examples in each iteration
-    batch = mnist.train.next_batch(100)
-    # replace placeholders with actual training examples
-    train_step.run(feed_dict = {x: batch[0], y_: batch[1]})
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], 
+                          padding='SAME')
 
-# Evaluating the Model
-# Checks whether the output y matches the target y_. Gives a list of booleans.
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-# Convert booleans to floats and take the mean to get accuracy
+# --------------------------First Convolutional Layer---------------------------
+
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+# -------------------------Second Convolutional Layer---------------------------
+
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+# ----------------------------Densely Connected Layer---------------------------
+
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# -----------------------------------Dropout------------------------------------
+
+# Applying dropout to reduce overfitting as explained in Dropout paper JMLR 2014
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+# ---------------------------------Readout layer--------------------------------
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+# ------------------------------Train and Evaluate------------------------------
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits
+    (labels=y_, logits=y_conv))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_:mnist.test.labels}))
+sess.run(tf.global_variables_initializer())
+for i in range(20000):
+    batch = mnist.train.next_batch(50)
+    if i%100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={
+            x:batch[0], y_: batch[1], keep_prob: 1.0})
+    print("step %d, training accuracy %g"%(i, train_accuracy))
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g"%accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
