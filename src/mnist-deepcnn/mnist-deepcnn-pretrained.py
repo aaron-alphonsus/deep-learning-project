c765c6115afa21ab01b353d162e814c8b75f5559
Aaron Alphonsus <aaron.alphonsus@gmail.com>
2018-04-22 20:03:58 -0600

    Save and restore working for deep cnn.

--- src/mnist-deepcnn/mnist-deepcnn-pretrained.py
+++ src/mnist-deepcnn/mnist-deepcnn-pretrained.py
@@ -3,7 +3,7 @@
 from tensorflow.examples.tutorials.mnist import input_data
 # mnist is a lightweight class that stores training, validation and testing sets
 # as NumPy arrays. Also provides function for iterating through data minibatches
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True) # one_hot = binary
+mnist = input_data.read_data_sets('./../MNIST_data', one_hot=True) 
  
 # ---------------------Start TensorFlow InteractiveSession----------------------
  
@@ -15,7 +15,7 @@ sess = tf.InteractiveSession() # Allows more flexible structuring of code
 # Create placeholder nodes for input images and target classes
 x = tf.placeholder(tf.float32, shape=[None, 784]) # 28*28 image linearized
 y_ = tf.placeholder(tf.float32, shape=[None, 10]) # digit class (0 to 9)
- 
+
 # -----------------------------Weight Initialization----------------------------
 
 # Initializing weights with a little noise
@@ -70,6 +70,7 @@ keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 # ---------------------------------Readout layer--------------------------------
+
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
 
@@ -77,19 +78,15 @@ y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 # ------------------------------Train and Evaluate------------------------------
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits
-    (labels=y_, logits=y_conv))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+# Creates the Saver object whose restore() function we will use later 
+saver = tf.train.Saver()
+
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.global_variables_initializer())
-for i in range(20000):
-    batch = mnist.train.next_batch(50)
-    if i%100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x:batch[0], y_: batch[1], keep_prob: 1.0})
-        print("step %d, training accuracy %g"%(i, train_accuracy))
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+# Restore the trained model
+saver.restore(sess, './savefiles/my-model')
+
+print("Test accuracy %g%%" % (accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})*100))
